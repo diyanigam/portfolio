aaab22bc801cb2860cab7a0d41048d9dfd7d63c8
diya <diyanigam02@gmail.com>
2025-06-01 23:00:10 +0530

    sprite animation v2

--- src/components/bot.jsx
+++ src/components/bot.jsx
@@ -3,16 +3,16 @@ import { motion } from "framer-motion";
 
 const MOVE_SPEED = 0.2; // Pixels per millisecond
 const FRAME_DURATION = 100;
-const STANDING_FRAME = "../src/assets/sprite/standing.png";
-const BLINK_FRAME = "./src/assets/sprite/blink.png";
+const STANDING_FRAME = "./public/assets/sprite/standing.png";
+const BLINK_FRAME = "./public/assets/sprite/blink.png";
 
 const spriteFrames = [
-  "../src/assets/sprite/standing.png",
-  "../src/assets/sprite/walking2.png",
-  "../src/assets/sprite/walking3.png",
-  "../src/assets/sprite/walking4.png",
-  "../src/assets/sprite/walking5.png",
-  "../src/assets/sprite/walking7.png",
+  "../public/assets/sprite/standing.png",
+  "../public/assets/sprite/walking2.png",
+  "../public/assets/sprite/walking3.png",
+  "../public/assets/sprite/walking4.png",
+  "../public/assets/sprite/walking5.png",
+  "../public/assets/sprite/walking7.png",
   
 ];
 
--- src/components/cursor.jsx
+++ src/components/cursor.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
-import cursorDefault from "../assets/cursor-default.PNG";
-import cursorHover from "../assets/cursor-hover.PNG";
-import cursorClick from "../assets/cursor-click.PNG";
+import cursorDefault from "/public/assets/cursor-default.PNG";
+import cursorHover from "/public/assets/cursor-hover.PNG";
+import cursorClick from "/public/assets/cursor-click.PNG";
 
 export default function CustomCursor() {
   const [position, setPosition] = useState({ x: 0, y: 0 });
